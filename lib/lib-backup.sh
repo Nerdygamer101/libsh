f738d61c72a86dd6512c9ca507930f13012c7904
Keanen <kramer2012@gmail.com>
2026-04-03 16:22:33 -0700

    Fix the dumb mistakes I was making in lib-backup causing exits

--- lib/lib-backup.sh
+++ lib/lib-backup.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 
-. lib-restic.sh
+# Use restic if the environment is set up
+if [ -n "${RESTIC_REPOSITORY}" ]; then
+	. lib-restic.sh
+	return 0
+fi
 
-backup_stdin() {
-	:
-}
+# If no supported backup tools are found, error out
+echo "ERROR: No supported backup tools setup" 1>&2
+echo "Currently supported tools: restic" 1>&2
+exit 1
